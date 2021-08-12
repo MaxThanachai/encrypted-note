762623c2d89318ad39f292f7545502d306aa224c
MaxThanachai <sobersobad@hotmail.com>
2021-08-12 23:41:20 +0700

    💄 redesign :smoking:

--- encrypted_note/lib/const/color.dart
+++ encrypted_note/lib/const/color.dart
@@ -14,3 +14,12 @@ const whisperGray = const Color(0xFFE9E8E8);
 const solitudeLightGray = const Color(0xFFF4F5F7);
 const solitudeMediumGray = const Color(0xFFE5E8EF);
 const seaGreen = const Color(0xFF219653);
+
+const blueBlack = const Color(0xFF13011B);
+const blueDark = const Color(0xFF170144);
+const blueNeon = const Color(0xFF090CF3);
+const blueWhite = const Color(0xFFD8F3FF);
+const redWhite = const Color(0xFFFFC2C2);
+const redNeon = const Color(0xFFFC0200);
+const redDark = const Color(0xFF44001D);
+const redBlack = const Color(0xFF2E0019);
--- encrypted_note/lib/screens/notepad/notepad.dart
+++ encrypted_note/lib/screens/notepad/notepad.dart
@@ -17,36 +17,33 @@ class _MyHomePageState extends State<MyHomePage> {
 
   bool isEncrypted = false;
 
-  final key = encrypt.Key.fromUtf8('4869502448695024');
   final iv = encrypt.IV.fromLength(8);
-  dynamic encrypter;
 
-  Color border = lightBlue;
-  Color textFieldColor = white;
-  Color background = whisperGray;
-
-  void filterTextInQuote() {
-    List<String> splittedStr = noteController.text.split('"');
-    for (int i = 0; i < splittedStr.length; i++) {
-      if (i % 2 != 0) print(splittedStr[i]);
-    }
-  }
+  Color textColor = blueWhite;
+  Color borderColor = blueNeon;
+  Color fillColor = blueDark;
+  Color backgroundColor = blueBlack;
 
   String encrypting(String plainText) {
-    print(plainText);
+    encrypt.Key key = encrypt.Key.fromUtf8(keyController.text);
+    encrypt.Encrypter encrypter =
+        encrypt.Encrypter(encrypt.AES(key, mode: encrypt.AESMode.ecb));
     return encrypter.encrypt(plainText, iv: iv).base64;
   }
 
   String decrypting(String encryptedText) {
-    print(encryptedText);
+    encrypt.Key key = encrypt.Key.fromUtf8(keyController.text);
+    encrypt.Encrypter encrypter =
+        encrypt.Encrypter(encrypt.AES(key, mode: encrypt.AESMode.ecb));
     encrypt.Encrypted encrypted = encrypt.Encrypted.from64(encryptedText);
     return encrypter.decrypt(encrypted, iv: iv);
   }
 
   void switchColorTheme() {
-    border = isEncrypted ? lightBlue : Colors.red;
-    textFieldColor = isEncrypted ? white : Colors.red[100] as Color;
-    background = isEncrypted ? whisperGray : Colors.red[300] as Color;
+    textColor = isEncrypted ? blueWhite : redWhite;
+    borderColor = isEncrypted ? blueNeon : redNeon;
+    fillColor = isEncrypted ? blueDark : redDark;
+    backgroundColor = isEncrypted ? blueBlack : redBlack;
   }
 
   void toggleEncrypting() {
@@ -78,37 +75,25 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
-    encrypter = encrypt.Encrypter(encrypt.AES(key, mode: encrypt.AESMode.ecb));
     super.initState();
   }
 
-  Widget keyBar() {
+  Widget appBar() {
     return Container(
-      height: 50,
-      margin: EdgeInsets.all(12),
-      padding: EdgeInsets.fromLTRB(12, 8, 12, 8),
-      decoration: BoxDecoration(
-        color: border,
-        borderRadius: BorderRadius.circular(12),
-      ),
+      height: 96,
+      width: 369,
+      margin: EdgeInsets.fromLTRB(12, 12, 12, 0),
       child: Row(
         children: [
-          Icon(
-            Icons.vpn_key,
-            color: textFieldColor,
-          ),
-          SizedBox(
-            width: 10,
-          ),
           Expanded(
-            child: Container(
-              padding: EdgeInsets.fromLTRB(5, 0, 5, 5),
-              decoration: BoxDecoration(
-                color: textFieldColor,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: TextFormField(
-                controller: keyController,
+            child: Center(
+              child: Text(
+                'NameTitle',
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
@@ -117,20 +102,71 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  Widget keyBar() {
+    return Container(
+      height: 108,
+      width: 369,
+      margin: EdgeInsets.fromLTRB(12, 0, 12, 0),
+      child: Center(
+        child: Container(
+          height: 36,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(48),
+            color: fillColor,
+            border: Border.all(
+              width: 2,
+              color: borderColor,
+            ),
+          ),
+          child: Row(
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              Icon(
+                Icons.vpn_key,
+                color: borderColor,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: TextFormField(
+                  controller: keyController,
+                  obscureText: true,
+                  style: TextStyle(
+                    color: textColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget notePad() {
     return Expanded(
       child: Container(
-        margin: EdgeInsets.all(12),
-        padding: EdgeInsets.all(12),
+        margin: EdgeInsets.fromLTRB(12, 0, 12, 12),
+        padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
         decoration: BoxDecoration(
-          color: textFieldColor,
-          borderRadius: BorderRadius.circular(12),
+          borderRadius: BorderRadius.circular(48),
+          border: Border.all(
+            width: 2,
+            color: borderColor,
+          ),
         ),
         child: TextFormField(
           enabled: !isEncrypted,
           controller: noteController,
           keyboardType: TextInputType.multiline,
           maxLines: null,
+          style: TextStyle(
+            color: textColor,
+          ),
         ),
       ),
     );
@@ -139,14 +175,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: background,
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: border,
-      ),
+      backgroundColor: backgroundColor,
+      extendBodyBehindAppBar: true,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          appBar(),
           keyBar(),
           notePad(),
         ],
@@ -154,8 +188,11 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: toggleEncrypting,
         tooltip: isEncrypted ? 'decrypt' : 'encrypt',
-        backgroundColor: border,
-        child: isEncrypted ? Icon(Icons.vpn_key) : Icon(Icons.lock),
+        backgroundColor: fillColor,
+        shape: new CircleBorder(
+          side: BorderSide(width: 2, color: textColor),
+        ),
+        child: isEncrypted ? Icon(Icons.lock_open) : Icon(Icons.lock_outline),
       ),
     );
   }
